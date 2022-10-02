fad7205c118768d68331b80ff5d380d0c14041ee
SCY <shen421226@gmail.com>
2022-10-02 14:27:58 +0800

    123

--- javascript/order.js
+++ javascript/order.js
@@ -18,19 +18,6 @@ sidebar.addEventListener('click', (e) => {
   sidebarbtns[1].ariaExpanded === "false" ? sidebarbtns[1].classList.remove('active') : sidebarbtns[1].classList.add('active');
 })
 
-// navbar active
-const nav = document.querySelector('.nav');
-const navbtns = document.querySelectorAll('.nav>li>a')
-console.log(navbtns);
-nav.addEventListener('click', (e) => {
-  const chosen = e.target.closest('a');
-  navbtns.forEach(btn => {
-    btn.classList.remove('active');
-  })
-
-  chosen.classList.add('active')
-});
-
 // 訂單刪除
 const deleteByModal = document.querySelector('#deleteModal');
 deleteByModal.addEventListener('show.bs.modal', (e) => {
